73a1f4ab83b86d0784eda0b2b2ad461eb65623ca
zkvarz <zkvarz@gmail.com>
2016-03-22 00:46:22 +0200

    api 15, facebook login id, token

--- app/src/main/java/com/example/myconnections_android/ui/activities/MainActivity.java
+++ app/src/main/java/com/example/myconnections_android/ui/activities/MainActivity.java
@@ -6,14 +6,33 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
 import com.example.myconnections_android.R;
+import com.example.myconnections_android.api.models.FacebookUserResponse;
+import com.example.myconnections_android.helpers.Logger;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
+import com.google.gson.Gson;
+
+import org.json.JSONObject;
+
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private  CallbackManager callbackManager;
+    private Button facebookLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,7 +48,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         Button calendarButton = (Button) findViewById(R.id.calendarButton);
         calendarButton.setOnClickListener(this);
 
-
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -38,6 +56,20 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                         .setAction("Action", null).show();
             }
         });
+        initializeFacebook();
+    }
+
+    private void initializeFacebook() {
+        facebookLogin = (Button) findViewById(R.id.facebookLogin);
+//        facebookLogin.setReadPermissions("public_profile");
+        facebookLogin.setOnClickListener(this);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+        Logger.debug(getClass(), "onActivityResult requestCode");
     }
 
     @Override
@@ -73,8 +105,52 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 Intent intent = new Intent(this, GoogleAnimationActivity.class);
                 startActivity(intent);
                 break;
-            case R.id.calendarButton:
-                startActivity(new Intent(this, CalendarActivity.class));
+            case R.id.facebookLogin:
+                Logger.debug(getClass(), "Click!");
+                // Initialize Facebook SDK
+                FacebookSdk.sdkInitialize(getApplicationContext());
+
+                callbackManager = CallbackManager.Factory.create();
+
+                LoginManager.getInstance().registerCallback(callbackManager,
+                        new FacebookCallback<LoginResult>() {
+                            @Override
+                            public void onSuccess(LoginResult loginResult) {
+                                Logger.debug(MainActivity.class.getClass(), "onSuccess!");
+                                Log.d("HEY", "onSuccess!!!");
+                                Log.d("TOKEN!", loginResult.getAccessToken().getToken());
+                                Log.d("TOKEN id!", loginResult.getAccessToken().getUserId());
+
+                                GraphRequest request = GraphRequest.newMeRequest(
+                                        loginResult.getAccessToken(),
+                                        new GraphRequest.GraphJSONObjectCallback() {
+                                            @Override
+                                            public void onCompleted(
+                                                    JSONObject object,
+                                                    GraphResponse response) {
+                                                Log.d("MY ID", "onSuccess!!!");
+                                                FacebookUserResponse facebookUserResponse = new Gson().fromJson(object.toString(), FacebookUserResponse.class);
+                                                Log.d("MY ID", "facebookUserResponse id" + facebookUserResponse.getId());
+                                                Log.d("MY ID", "facebookUserResponse name" + facebookUserResponse.getName());
+                                            }
+                                        });
+                                Bundle parameters = new Bundle();
+                                parameters.putString("fields", "id,name,link");
+                                request.setParameters(parameters);
+                                request.executeAsync();
+                            }
+
+                            @Override
+                            public void onCancel() {
+                                Logger.debug(MainActivity.class.getClass(), "onCancel!");
+                            }
+
+                            @Override
+                            public void onError(FacebookException error) {
+                                Logger.debug(getClass(), "Error! " + error.getMessage());
+                            }
+                        });
+                LoginManager.getInstance().logInWithReadPermissions(MainActivity.this, Arrays.asList("public_profile", "user_friends"));
                 break;
 
         }
